f5c88480fe4d637d7625000744014ccfd00d541d
Ryan Echols <rkechols21@gmail.com>
2024-11-30 22:49:03 -0700

    solved day 1

--- day01.py
+++ day01.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from advent2024!")
+from collections import Counter
+
+from advent_utils import read_input
+
+
+InputData = list[tuple[int, int]]
+
+
+def get_parsed_input() -> InputData:
+    input_raw = read_input(1)
+    data = [
+        tuple(map(int, line.split()))
+        for line in input_raw.strip().split("\n")
+    ]
+    return data
+
+
+def main(input_parsed: InputData):
+    # part 1
+    list1, list2 = map(sorted, zip(*input_parsed))
+    total_diff = sum(abs(a - b) for a, b in zip(list1, list2))
+    print(f"{total_diff = }")
+    # part 2
+    counter = Counter(list2)
+    total_similarity = sum(
+        a * counter.get(a, 0)
+        for a in list1
+    )
+    print(f"{total_similarity = }")
 
 
 if __name__ == "__main__":
-    main()
+    main(get_parsed_input())
