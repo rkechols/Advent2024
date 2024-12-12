38e31b5bee3f0d92c13584740af8a2b9fc26473d
Ryan Echols <rkechols21@gmail.com>
2024-12-11 23:33:55 -0700

    solved day 12

--- advent_utils.py
+++ advent_utils.py
@@ -1,7 +1,9 @@
 import sys
 import time
 from contextlib import contextmanager
+from enum import Enum
 from pathlib import Path
+from typing import NamedTuple, Self
 
 DATA_DIR = Path(__file__).parent / "data"
 
@@ -19,3 +21,22 @@ def timer():
     finally:
         duration = time.perf_counter() - time_start
         print(f"[{duration:.3f} seconds]", file=sys.stderr)
+
+
+class Direction(Enum):
+    UP = (-1, 0)
+    RIGHT = (0, 1)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+
+    def perpendiculars(self) -> tuple[Self, Self]:
+        a, b = self.value
+        return Direction((b, a)), Direction((-b, -a))
+
+
+class Loc(NamedTuple):
+    row: int
+    col: int
+
+    def shift(self, direction: Direction) -> Self:
+        return Loc(self.row + direction.value[0], self.col + direction.value[1])
---
+++ day12.py
@@ -0,0 +1,116 @@
+import dataclasses
+from typing import NamedTuple
+
+import numpy as np
+
+from advent_utils import Direction, Loc, read_input, timer
+
+InputData = np.ndarray
+
+
+def get_parsed_input() -> InputData:
+    input_raw = read_input(12)
+    grid = np.array([
+        list(line.strip())
+        for line in input_raw.strip().splitlines()
+    ])
+    return grid
+
+
+class Fence(NamedTuple):
+    loc: Loc
+    direction: Direction
+
+
+@dataclasses.dataclass
+class Region:
+    area: set[Loc]
+    perimeter: set[Fence]
+
+    def get_region_cost(self, *, bulk_discount: bool) -> int:
+        if not bulk_discount:
+            return len(self.area) * len(self.perimeter)
+        n_sides = 0
+        all_counted_fences = set()
+        for fence_initial in self.perimeter:
+            if fence_initial in all_counted_fences:
+                continue
+            this_side = {fence_initial}
+            fences_to_check = {fence_initial}
+            while len(fences_to_check) > 0:
+                fences_to_check_next = set()
+                for fence_to_check in fences_to_check:
+                    for shift_direction in fence_to_check.direction.perpendiculars():
+                        adjacent_loc_inline = fence_to_check.loc.shift(shift_direction)
+                        if (adjacent_fence_inline := Fence(adjacent_loc_inline, fence_to_check.direction)) in self.perimeter:
+                            if adjacent_fence_inline not in this_side:
+                                fences_to_check_next.add(adjacent_fence_inline)
+                            this_side.add(adjacent_fence_inline)
+                fences_to_check = fences_to_check_next
+            all_counted_fences.update(this_side)
+            n_sides += 1
+        return len(self.area) * n_sides
+
+
+class RegionFinder:
+    def __init__(self, grid: np.ndarray):
+        super().__init__()
+        if len(grid.shape) != 2:
+            raise ValueError("grid must be 2-dimensional")
+        self.grid = grid
+
+    @property
+    def n_rows(self) -> int:
+        return self.grid.shape[0]
+
+    @property
+    def n_cols(self) -> int:
+        return self.grid.shape[1]
+
+    def is_loc_in_bounds(self, loc: Loc) -> bool:
+        return 0 <= loc.row < self.n_rows and 0 <= loc.col < self.n_cols
+
+    def find_regions(self) -> list[Region]:
+        to_return = []
+        all_searched_locs = set()
+        for i in range(self.n_rows):
+            for j in range(self.n_cols):
+                region_start_loc = Loc(i, j)
+                if region_start_loc in all_searched_locs:
+                    continue
+                this_symbol = self.grid[region_start_loc]
+                area = set()
+                perimeter = set()
+                to_search = {region_start_loc}
+                while len(to_search) > 0:
+                    to_search_new = set()
+                    for loc in to_search:
+                        if loc in area:  # already analyzed this one
+                            continue
+                        area.add(loc)
+                        for direction in Direction:
+                            adjacent_loc = loc.shift(direction)
+                            if self.is_loc_in_bounds(adjacent_loc):
+                                if self.grid[adjacent_loc] == this_symbol:
+                                    to_search_new.add(adjacent_loc)
+                                else:
+                                    perimeter.add(Fence(loc, direction))
+                            else:
+                                perimeter.add(Fence(loc, direction))
+                    to_search = to_search_new
+                to_return.append(Region(area, perimeter))
+                all_searched_locs.update(area)
+        return to_return
+
+
+def main(input_parsed: InputData):
+    regions = RegionFinder(input_parsed).find_regions()
+    cost1 = sum(region.get_region_cost(bulk_discount=False) for region in regions)
+    print(f"{cost1 = }")
+    cost2 = sum(region.get_region_cost(bulk_discount=True) for region in regions)
+    print(f"{cost2 = }")
+
+
+if __name__ == "__main__":
+    with timer():
+        main(get_parsed_input())
