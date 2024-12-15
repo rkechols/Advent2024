f8a8f7dadba3b16ddf0256f6cec4d22bb440233c
Ryan Echols <rkechols21@gmail.com>
2024-12-15 02:35:23 -0700

    vectorize day 14

--- day14.py
+++ day14.py
@@ -7,61 +7,50 @@ from pathlib import Path
 import numpy as np
 from PIL import Image
 
-from advent_utils import Direction, Loc, read_input, timer
+from advent_utils import read_input, timer
 
-BotsState = dict[Loc, set[Direction]]
+InputData = np.ndarray
 
 RENDERINGS_DIR = Path("data/day14-renderings")
 shutil.rmtree(RENDERINGS_DIR, ignore_errors=True)
 RENDERINGS_DIR.mkdir(parents=True, exist_ok=True)
 
 
-def get_parsed_input() -> BotsState:
+def get_parsed_input() -> InputData:
     input_raw = read_input(14)
-    input_parsed = []
-    for line in input_raw.strip().splitlines():
-        px, py, vx, vy = map(int, re.fullmatch(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)", line).groups())
-        input_parsed.append((Loc(px, py), Direction(vx, vy)))
-    input_structured: BotsState = defaultdict(set)
-    for loc, direction in input_parsed:
-        input_structured[loc].add(direction)
-    return input_structured
+    input_parsed = np.array([
+        list(map(int, re.fullmatch(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)", line).groups()))
+        for line in input_raw.strip().splitlines()
+    ])
+    return input_parsed
 
 
 class Solver:
-    def __init__(self, n_rows: int, n_cols: int, bots: BotsState):
+    def __init__(self, n_rows: int, n_cols: int, bots_data: InputData):
         if n_rows <= 0 or n_cols <= 0:
             raise ValueError("grid dimensions must be positive numbers")
         if n_rows % 2 == 0 or n_cols % 2 == 0:
             raise ValueError("grid dimensions must both be odd numbers")
         self.n_rows = n_rows
         self.n_cols = n_cols
-        self.all_bots = bots
+        if len(bots_data.shape) != 2 or bots_data.shape[1] != 4:
+            raise ValueError("bots array must be of shape (n, 4)")
+        self.bot_locs = bots_data[:, :2]
+        self.bot_directions = bots_data[:, 2:]
         self._state_history: set[str] = set()
         self._found_loop = False
         self.seconds_elapsed = 0
         self.save_rendering()
 
     def save_rendering(self):
-        canvas = np.array([
-            [
-                len(self.all_bots[Loc(i, j)]) > 0
-                for j in range(self.n_cols)
-            ]
-            for i in range(self.n_rows)
-        ])
+        canvas = np.zeros((self.n_rows, self.n_cols), dtype=bool)
+        for bot_loc in self.bot_locs:
+            canvas[*bot_loc] = True
         image = Image.fromarray(canvas.transpose())
         image.save(RENDERINGS_DIR / f"{self.seconds_elapsed:09d}-seconds.png")
 
     def get_state_str(self) -> str:
-        lines = []
-        for loc, bots in sorted(self.all_bots.items()):
-            if len(bots) == 0:
-                continue
-            loc_str = ",".join(map(str, loc))
-            loc_bots = " ".join(",".join(map(str, bot)) for bot in sorted(bots))
-            lines.append(f"{loc_str}|{loc_bots}")
-        return "\n".join(lines)
+        return "\n".join(" ".join(map(str, bot_loc)) for bot_loc in self.bot_locs)
 
     def simulate(self, n_seconds_stop: int | None):
         if self._found_loop:
@@ -72,21 +61,17 @@ class Solver:
                 return
             elif n_seconds_stop < self.seconds_elapsed:
                 raise ValueError("simulation has already run past that point; use a new Solver instance")
+        position_mod = np.array([self.n_rows, self.n_cols])
         while n_seconds_stop is None or self.seconds_elapsed < n_seconds_stop:
             # progress forward 1 second
-            all_bots_new: BotsState = defaultdict(set)
-            for loc, bots in self.all_bots.items():
-                for bot_direction in bots:
-                    bot_new_loc = loc.shift(bot_direction)
-                    bot_new_loc = Loc(bot_new_loc.row % self.n_rows, bot_new_loc.col % self.n_cols)
-                    all_bots_new[bot_new_loc].add(bot_direction)
-            self.all_bots = all_bots_new
+            self.bot_locs += self.bot_directions
+            self.bot_locs %= position_mod
             self.seconds_elapsed += 1
             # check if we're in a loop
             state_str = self.get_state_str()
             if state_str in self._state_history:  # seen this state before!
                 self._found_loop = True
-                print(f"after {self.seconds_elapsed} seconds, the bots have started repeating formations")
+                print(f"after {self.seconds_elapsed} iterations, the bots have started repeating formations")
                 return
             # new state: save it
             self._state_history.add(state_str)
@@ -96,20 +81,20 @@ class Solver:
         quadrant_line_rows = self.n_rows // 2
         quadrant_line_cols = self.n_cols // 2
         quadrant_counts = defaultdict(int)
-        for loc, bots in self.all_bots.items():
-            if loc.row == quadrant_line_rows:
+        for row, col in self.bot_locs:
+            if row == quadrant_line_rows:
                 continue
-            row_bool = loc.row < quadrant_line_rows
-            if loc.col == quadrant_line_cols:
+            row_bool = row < quadrant_line_rows
+            if col == quadrant_line_cols:
                 continue
-            col_bool = loc.col < quadrant_line_cols
-            quadrant_counts[(row_bool, col_bool)] += len(bots)
+            col_bool = col < quadrant_line_cols
+            quadrant_counts[(row_bool, col_bool)] += 1
         return functools.reduce(lambda a, b: a * b, quadrant_counts.values(), 1)
 
 
-def main(bots: BotsState):
+def main(input_parsed: InputData):
     # part 1
-    solver = Solver(101, 103, bots)
+    solver = Solver(101, 103, input_parsed)
     solver.simulate(n_seconds_stop=100)
     score = solver.get_score()
     print(f"{score = }")
